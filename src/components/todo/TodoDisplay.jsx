fde7063eb193160b27a231824835ab2692e8b47c
NguyenTheAnh0502 <s3975844@rmit.edu.vn>
2025-07-13 00:23:32 +0700

    video 39 16:38

--- src/components/todo/TodoDisplay.jsx
+++ src/components/todo/TodoDisplay.jsx
@@ -3,15 +3,22 @@ const TodoDisplay = (props) => {
     //console.log("check props: " , props);
     
     //method 1: popular
-    const{name, age, data, todoList} = props;
+    const{todoList} = props;
 
     //method 2: beginner const variableName = props.key;
     //method 3: take directly {name} lazy
     return(
         <div className="d-flex flex-column justify-content-center text-center">
-          <div>My name is {name}</div>
-          <div >Learning React</div>
-          <div>{JSON.stringify(todoList)}</div>
+          {todoList.map((item, index) => {
+            
+            return (
+              <div className="todoDisplay">
+                <div>{item.name}</div>
+                <button>delete</button>
+              </div>
+            )
+          })}
+          
         </div>
     )
 }
--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -5,7 +5,7 @@ const TodoInput = (props) =>{
     //useState hook
     //const valueUserInput = "";
     //destructuring Array
-    const [valueUserInput, setValueInput] = useState("The Anh")
+    const [valueUserInput, setValueInput] = useState("")
     console.log(">>> check point: ", props);
     const {addNewTodo} = props;
     //addNewTodo();
@@ -14,6 +14,7 @@ const TodoInput = (props) =>{
     const handleClick = () =>{
       console.log("check: ", valueUserInput);
       addNewTodo(valueUserInput);
+      setValueInput("");
     }
 
     const handleOnchange = (name) =>{
@@ -23,12 +24,14 @@ const TodoInput = (props) =>{
     //onChange ={() => function} using arrow function for the function that have a diffrent parameter
     return(
         <div className="d-flex justify-content-center mt-2">
-          <input type="text" placeholder="Enter your task" className="form-control-sm"
+          <input 
+          type="text" placeholder="Enter your task" className="form-control-sm"
           onChange={(event) => handleOnchange(event.target.value)}
+
+          value = {valueUserInput}
           ></input>
 
           <button className="btn btn-primary" onClick={handleClick}>Add</button>
-          <div>My text input = {valueUserInput}</div>
         </div>
     );
 }
