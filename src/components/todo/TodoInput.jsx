9d2b42705a62d459c2700d1e01a4db85618d3531
NguyenTheAnh0502 <s3975844@rmit.edu.vn>
2025-07-12 23:03:27 +0700

    Video 36 method 1 event

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -3,10 +3,21 @@ const TodoInput = (props) =>{
     console.log(">>> check point: ", props);
     const {addNewTodo} = props;
     //addNewTodo();
+
+
+    const handleClick = () =>{
+      alert("handle click button")
+    }
+
+    const handleOnchange = (event) =>{
+      console.log("handlOnChange", event.target.value);
+    }
     return(
         <div className="d-flex justify-content-center mt-2">
-          <input type="text" placeholder="Enter your task" className="form-control-sm"></input>
-          <button className="btn btn-primary">Add</button>
+          <input type="text" placeholder="Enter your task" className="form-control-sm"
+          onChange={handleOnchange}
+          ></input>
+          <button className="btn btn-primary" onClick={handleClick}>Add</button>
         </div>
     );
 }
