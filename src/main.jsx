22c61e74cfc25f288d3a59a3cf66b9a483297284
NguyenTheAnh0502 <s3975844@rmit.edu.vn>
2025-07-26 22:15:24 +0700

    video 45 10:07

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';//step 1
+
+const router = createBrowserRouter([ //step 2
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/login",
+    element:<div>login</div>
+  },
+  {
+    path: "/register",
+    element:<div>login</div>
+  },
+  {
+    path: "/users",
+    element:<div>User</div>
+  },
+  {
+    path: "/products",
+    element:<div>product</div>
+  }
+])
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* step 3 */}
+    <RouterProvider router= {router}/> 
   </StrictMode>,
 )
